2d7b37fb450f3ea7e798a412be269226f2398ffa
xu_yanfeng <xu_yanfeng@126.com>
2024-03-14 17:14:21 +0800

    代码加密

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 var lib = require("jsfuck");
 const { join } = require("path")
 const { readFileSync, writeFileSync } = require("fs");
-var data = readFileSync(join(__dirname, './1.js'), "utf8");
-var output = lib.JSFuck.encode(data, false);
- 
-writeFileSync(join(__dirname, './1.en.js'), output, "utf8");
+
+
+function fuck(data) {
+    const output = lib.JSFuck.encode(data, false);
+    return output;
+}
+
+function obf(data) {
+    const JavaScriptObfuscator = require('javascript-obfuscator');
+    const obfuscationResult = JavaScriptObfuscator.obfuscate(data, {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 1,
+        numbersToExpressions: true,
+        simplify: true,
+        stringArrayShuffle: true,
+        splitStrings: true,
+        stringArrayThreshold: 1
+    });
+    const encode = obfuscationResult.getObfuscatedCode();
+    return encode;
+}
+function main(file) {
+    let data = readFileSync(join(__dirname, `./${file}.js`), "utf8");
+    data = obf(data);
+    data = obf(data);
+    // data = fuck(data);
+    writeFileSync(join(__dirname, `./${file}.en.js`), data, "utf8");
+}
+main('1');
